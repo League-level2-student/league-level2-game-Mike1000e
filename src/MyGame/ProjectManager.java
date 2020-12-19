9b774eafd49349518db998e7ba18fab55effd81f
Mike1000e <denis.luckash@gmail.com>
2020-12-18 18:27:34 -0800

    Game Start

---
+++ src/MyGame/ProjectManager.java
@@ -0,0 +1,11 @@
+package MyGame;
+
+public class ProjectManager {
+		public static void main(String [] args) {
+			for(int i = 0; i<10; i++) {
+			System.out.println("hello");
+			}
+			
+			
+		}
+}
